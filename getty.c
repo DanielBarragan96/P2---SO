8226fdb1f7d6d2c73b9ecfc1a1dbe27025095daf
DanielBarragan96 <ie702554@iteso.mx>
2018-02-05 14:57:59 -0600

    Corrected nameS incrementing in each excecution

--- getty.c
+++ getty.c
@@ -11,21 +11,21 @@ char* concat(char s1[], char s2[]);
 int readTxtFile(char * fname);
 int comparar_string(char t1[], char t2[]);
 
-char name[20];
-char password [20]; 
-char* nameS;
-
 int main()
 {
 	while(1)
 	{
+        char name[20] = "";
+        char password [20] = ""; 
+        char* nameS = "";
+
 		printf("Nombre de usuario: ");
 		scanf("%s",name);
 		char puntos[]={":"};
 		nameS = concat(name,puntos);
 		printf("Contrasena: ");
 		scanf("%s",password);
-		nameS = concat(name,password);
+		nameS = concat(nameS,password);
 		printf("%s\n",nameS);
 
 	 	if(readTxtFile(nameS))
@@ -33,10 +33,15 @@ int main()
 			p = fork();
 			if(!p)
 			{
-				execlp("sh","sh",NULL);
+				execlp("./sh","sh",NULL);
 			}
 			else
 				wait(&status);
+				status = status >> 8;
+				if(2 == status)
+				{
+					return 0;
+				}
 		}
 		else
 		{
--- init.c
+++ init.c
@@ -3,50 +3,105 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int p;
+int p [6];
 int status;
+static int shut = 0;
+
+int searchDeadChild();
+int shutChild();
 
 int main()
 {
-p = fork();
-		if(!p)//hijo uno
+        int pipefd[2];
+        pipe(pipefd);
+		p[0] = fork();
+		if(!p[0])//hijo uno
 		{	
 			execlp("/usr/bin/xterm","xterm","-e","./getty",NULL);
 		}
-p = fork();
-		if(!p)//hijo dos
+		p[1] = fork();
+		if(!p[1])//hijo dos
 		{	
 			execlp("/usr/bin/xterm","xterm","-e","./getty",NULL);
 		}
-p = fork();
-		if(!p)//hijo tres
+		p[2] = fork();
+		if(!p[2])//hijo tres
 		{	
 			execlp("/usr/bin/xterm","xterm","-e","./getty",NULL);
 		}
-p = fork();
-		if(!p)//hijo cuatro
+		p[3] = fork();
+		if(!p[3])//hijo cuatro
 		{	
 			execlp("/usr/bin/xterm","xterm","-e","./getty",NULL);
 		}
-p = fork();
-		if(!p)//hijo cinco
+		p[4] = fork();
+		if(!p[4])//hijo cinco
 		{	
 			execlp("/usr/bin/xterm","xterm","-e","./getty",NULL);
 		}
-p = fork();
-		if(!p)//hijo seis
+		p[5] = fork();
+		if(!p[5])//hijo seis
 		{	
 			execlp("/usr/bin/xterm","xterm","-e","./getty",NULL);
 		}
+        printf("Procesos: \n %d \n",p[0]);
+        printf("%d \n",p[1]);
+        printf("%d \n",p[2]);
+        printf("%d \n",p[3]);
+        printf("%d \n",p[4]);
+        printf("%d \n",p[5]);
 
 	while(1)
 	{
 		wait(&status);
-		p = fork();
-		if(!p)//hijo uno
-		{	
-			execlp("/usr/bin/xterm","xterm","-e","./getty",NULL);
+		if(1 == shut)
+		{
+			kill(p[0], SIGKILL);
+            kill(p[1], SIGKILL);
+            kill(p[2], SIGKILL);
+            kill(p[3], SIGKILL);
+            kill(p[4], SIGKILL);
+            kill(p[5], SIGKILL);
+
+			return 1;
 		}
+        else
+        {
+            int deadChild = searchDeadChild();
+            //printf("Dead Child: %d\n",deadChild);
+            //printf("Process: %d Killed\n", p[deadChild]);
+		    p[deadChild] = fork();
+		    if(!p[deadChild])//hijo uno
+		    {	
+			    execlp("/usr/bin/xterm","xterm","-e","./getty",NULL);
+		    }
+        }
 	}
 	return 0;
 }
+
+int searchDeadChild()
+{
+    printf("pid: %d : %d \n",p[0], getpgid(p[0]));
+    printf("pid: %d : %d \n",p[1], getpgid(p[1]));
+    printf("pid: %d : %d \n",p[2], getpgid(p[2]));
+    printf("pid: %d : %d \n",p[3], getpgid(p[3]));
+    printf("pid: %d : %d \n",p[4], getpgid(p[4]));
+    printf("pid: %d : %d \n",p[5], getpgid(p[5]));
+    printf("-------------\n");
+
+    for(int i = 0; i<6; i++)
+    {
+        if(-1 == getpgid(p[i]))
+        {
+            return i;
+        }
+    }
+    return 6;
+}
+
+int shutChild()
+{
+    shut = 1;
+}
+
